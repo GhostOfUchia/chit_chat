157aa559ee011a018c0390c72df5b1ff66e48048
amit saini <amitsaini8484448@gmail.com>
2025-01-08 17:00:41 +0530

    Chat opage complete and Home Page start but nit running

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:chit_chat/models/chatroom_model.dart';
+import 'package:chit_chat/models/firebase_helper.dart';
 import 'package:chit_chat/models/user_model.dart';
 import 'package:chit_chat/pages/login_page.dart';
 import 'package:chit_chat/pages/search_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -45,11 +48,60 @@ class _HomePageState extends State<HomePage> {
         },
         child: const Icon(Icons.search),
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Text("home page"),
+      body: SafeArea(
+          child: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("chatrooms")
+              .where("participants.${widget.userModel.uid}", isEqualTo: true)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.hasData) {
+                QuerySnapshot chatRoomSnapShot = snapshot.data as QuerySnapshot;
+                return ListView.builder(
+                  itemCount: chatRoomSnapShot.docs.length,
+                  itemBuilder: (context, index) {
+                    ChatRoomModel chatRoomModel = ChatRoomModel.fromMap(
+                        chatRoomSnapShot.docs[index].data()
+                            as Map<String, dynamic>);
+                    Map<String, dynamic> participants =
+                        chatRoomModel.participants!;
+                    List<String> participantsKeys = participants.keys.toList();
+                    participantsKeys.remove(widget.userModel.uid);
+
+                    return FutureBuilder(
+                        future: FireBaseHelper.getUserById(participantsKeys[0]),
+                        builder: (context, userData) {
+                          UserModel targetUser = userData.data as UserModel;
+                          return ListTile(
+                            leading: const CircleAvatar(
+                              child: Icon(Icons.person),
+                            ),
+                            title: Text(targetUser.fullname.toString()),
+                            subtitle:
+                                Text(chatRoomModel.lastMessage.toString()),
+                          );
+                        });
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text(snapshot.hasError.toString()),
+                );
+              } else {
+                return const Center(
+                  child: Text("No Chats Availables"),
+                );
+              }
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
-      ),
+      )),
     );
   }
 }
